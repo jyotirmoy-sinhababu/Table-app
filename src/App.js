eeca3ea9e78d29448f7d3ac7319d06ce0e2cc8cf
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-11-28 16:19:44 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,123 @@ function App() {
     role: 'Role',
   };
 
+  //style of first table
+  const nameStyle = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '8px 10px',
+    gap: '10px',
+    position: 'absolute',
+    width: '175px',
+    height: '40px',
+    left: '0px',
+    top: '40px',
+    background: '#F5F5F5',
+    borderWidth: '0px 1px 1px 1px',
+    borderStyle: 'solid',
+    borderColor: ' #E1E1E1',
+  };
+
+  const emailStyle = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '8px 10px',
+    gap: '10px',
+    position: 'absolute',
+    width: '220px',
+    height: '40px',
+    left: '175px',
+    top: '40px',
+    background: '#F5F5F5',
+    borderWidth: '0px 1px 1px 0px',
+    borderStyle: 'solid',
+    borderColor: '#E1E1E1',
+  };
+
+  const roleStyle = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: ' 8px 10px',
+    gap: '10px',
+    position: 'absolute',
+    width: '175px',
+    height: '40px',
+    left: '395px',
+    top: '40px',
+    background: '#F5F5F5',
+    borderWidth: '0px 1px 1px 0px',
+    borderStyle: 'solid',
+    borderColor: '#E1E1E1',
+  };
+
+  const joiningStyle = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '8px 10px',
+    gap: '10px',
+    position: 'absolute',
+    width: '175px',
+    height: '40px',
+    left: '220px',
+    top: '80px',
+    background: '#FFFFFF',
+    borderWidth: '0px 1px 1px 0px',
+    borderStyle: 'solid',
+    borderColor: '#E1E1E1',
+  };
+
+  const cityStyle = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '8px 10px',
+    gap: '10px',
+    position: 'absolute',
+    width: '175px',
+    height: '40px',
+    left: '175px',
+    top: '40px',
+    background: '#F5F5F5',
+    borderWidth: '0px 1px 1px 0px',
+    borderStyle: 'solid',
+    borderColor: '#E1E1E1',
+  };
+
+  const imgStyle = {
+    boxSizing: 'border-box',
+    width: '25px',
+    height: '25px',
+    background: 'url()',
+    border: '1px solid #747474',
+    borderRadius: '40px',
+    flex: 'none',
+    order: '0',
+    flexGrow: '0',
+  };
+
+  const txtDataStyle = {
+    width: '69px',
+    height: '24px',
+    fontFamily: 'Noto Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '170%',
+    color: '#383838',
+    flex: 'none',
+    order: '1',
+    flexGrow: '0',
+  };
+
   useEffect(() => {
     if (data) {
       setLoopData(
--- src/component/table/TableComp.js
+++ src/component/table/TableComp.js
@@ -1,32 +1,71 @@
-import React from 'react';
+import './tablecomp.css';
 
 const TableComp = ({ arr, header }) => {
   return (
     <table className='table-cnt'>
-      <thead>
-        <tr>
-          {header.name ? <th>{header.name}</th> : null}
-          {header.email ? <th>{header.email}</th> : null}
-          {header.city ? <th>{header.city}</th> : null}
+      <thead className='table-hd'>
+        <tr className='table-hd-tr'>
+          {header.name ? (
+            <th className='table-hd'>
+              {' '}
+              <p>{header.name}</p>{' '}
+            </th>
+          ) : null}
+          {header.email ? (
+            <th className='table-hd'>
+              {' '}
+              <p>{header.email}</p>
+            </th>
+          ) : null}
+          {header.city ? (
+            <th className='table-hd'>
+              <p>{header.city}</p>
+            </th>
+          ) : null}
 
-          {header.joining ? <th>{header.joining}</th> : null}
-          {header.role ? <th>{header.role}</th> : null}
+          {header.joining ? (
+            <th className='table-hd'>
+              <p> {header.joining}</p>
+            </th>
+          ) : null}
+          {header.role ? (
+            <th className='table-hd'>
+              <p>{header.role}</p>
+            </th>
+          ) : null}
         </tr>
       </thead>
-      <tbody>
+      <tbody className='table-body'>
         {arr &&
           arr.map((item) => {
             return (
-              <tr>
+              <tr className='table-body-tr'>
                 {item?.name && item?.img && (
-                  <td>
-                    <img src={item?.img} alt='avataar' /> {item?.name}
+                  <td className='table-body-data'>
+                    <img src={item?.img} alt='avataar' />
+                    <p>{item?.name}</p>
                   </td>
                 )}
-                {item?.email && <td>{item?.email}</td>}
-                {item?.role && <td>{item?.role}</td>}
-                {item?.city && <td>{item.city}</td>}
-                {item?.joining && <td> {item.joining} </td>}
+                {item?.email && (
+                  <td className='table-body-data'>
+                    <p>{item?.email}</p>
+                  </td>
+                )}
+                {item?.city && (
+                  <td className='table-body-data'>
+                    <p>{item.city}</p>
+                  </td>
+                )}
+                {item?.joining && (
+                  <td className='table-body-data'>
+                    <p>{item.joining}</p>{' '}
+                  </td>
+                )}
+                {item?.role && (
+                  <td className='table-body-data'>
+                    <p> {item?.role}</p>
+                  </td>
+                )}
               </tr>
             );
           })}
